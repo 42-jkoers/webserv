2a669424a9992783d9f4d3ba5ca305db951d48c2
SirMorfield <jkoers@student.codam.nl>
2022-02-11 15:18:04 +0100

    feat: send message with response code a parameter

--- include/main.hpp
+++ include/main.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -14,8 +15,20 @@ void e_perror(const std::string& msg);
 
 } // namespace exit_with
 
+namespace cpp11 {
+
+template <typename T>
+std::string to_string(T value) {
+	std::stringstream ss;
+	ss << value;
+	return ss.str();
+}
+
+} // namespace cpp11
+
 fd_t		create_socket();
 void		listen_on_socket(fd_t fd, unsigned int port, struct sockaddr_in& address);
 fd_t		accept_from_fd(fd_t fd, const struct sockaddr_in& address);
 std::string read_request(fd_t fd);
-void		write_response(fd_t fd, const std::string& message);
+void		response(fd_t fd, uint32_t response_code, const std::string& message);
+std::string get_client_address(struct sockaddr_in& address);
---
+++ src/response.cpp
@@ -0,0 +1,31 @@
+#include "main.hpp"
+#include <map>
+#include <netinet/in.h>
+
+// TODO: optimize
+void response(fd_t fd, uint32_t response_code, const std::string& message) {
+	std::map<uint32_t, std::string> m;
+	m[200] = "OK";
+	m[201] = "Created";
+	m[202] = "Accepted";
+	m[204] = "No Content";
+	m[301] = "Moved Permanently";
+	m[302] = "Moved Temporarily";
+	m[304] = "Not Modified";
+	m[400] = "Bad Request";
+	m[401] = "Unauthorized";
+	m[403] = "Forbidden";
+	m[404] = "Not Found";
+	m[500] = "Internal Server Error";
+	m[501] = "Not Implemented";
+	m[502] = "Bad Gateway";
+	m[503] = "Service Unavailable";
+
+	std::string response = "HTTP/1.0 ";
+	response += cpp11::to_string(response_code);
+	response += " ";
+	response += m[response_code];
+	response += "\r\n\r\n";
+	response += message;
+	write(fd, response.c_str(), response.length());
+}

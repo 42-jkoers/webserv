3f5110c375f9a66ddbf4628da9ae5cb4ad3e7ad1
SirMorfield <jkoers@student.codam.nl>
2022-03-17 11:21:35 +0100

    feat: add write_file()

--- include/file_system.hpp
+++ include/file_system.hpp
@@ -5,7 +5,19 @@
 
 namespace fs {
 
-std::string				 read_file(const std::string& path);
+std::string read_file(const std::string& path);
+
+template <typename T>
+bool write_file(const std::string& path, const std::vector<T>& data) { // returns true on success
+	std::fstream file;
+	file.open(path.c_str());
+	if (!file.is_open())
+		return false;
+	file.write(data.data(), data.size() * sizeof(T));
+	file.close();
+	return true;
+}
+
 std::vector<std::string> list_dir(const std::string& path);
 
 } // namespace fs

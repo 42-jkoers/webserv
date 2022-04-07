1bc23d4c9382aa5cd7ee097ab3fa543e07513ec2
SirMorfield <jkoers@student.codam.nl>
2022-04-07 19:12:06 +0200

    refactor: moving Client class to dedicated file

--- include/poller.hpp
+++ include/poller.hpp
@@ -1,63 +1,8 @@
 #pragma once
+#include "client.hpp"
 #include "main.hpp"
 #include "request.hpp"
 #include "response.hpp"
-
-class Client {
-  public:
-	Client();
-	enum Read_status {
-		UNSET,
-		TEMPORALLY_UNIAVAILABLE,
-		IN_PROGRESS,
-		DONE
-	};
-
-	enum Body_type {
-		EMPTY,
-		MULTIPART,
-		CHUNKED
-	};
-
-	enum Parse_status {
-		INCOMPLETE = 0,
-		HEADER_IN_PROGRESS = 1,
-
-		HEADER_DONE = 2,
-		// send 100-continue
-		WAITING_FOR_BODY = 4,
-		// while (read() > 0)
-		BODY_IN_PROGRESS = 5,
-
-		BODY_DONE = 6,
-		FINISHED = 7,
-
-		ERROR = 8
-	};
-
-	enum Chunk_status {
-		CS_IN_PROGRESS,
-		CS_NULL_BLOCK_REACHED,
-		CS_ERROR
-	};
-
-	Read_status	 read_pollfd(const pollfd& pfd);
-	Parse_status parse_status() const;
-	void		 reset();
-
-	Request		 request;
-
-  private:
-	Read_status		  _read_status;
-	Parse_status	  _parse_status;
-	Body_type		  _body_type;
-	ssize_t			  _bytes_to_read;
-	std::vector<char> _buf;
-
-	bool			  _is_end_of_http_request(const std::string& s);
-	void			  _parse(size_t bytes_read, const pollfd& pfd);
-	Chunk_status	  _append_chunk(size_t bytes_read);
-};
 
 namespace constructors {
 

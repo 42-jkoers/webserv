58beffc90790b3943d1eafc1cecdf23c6de6b38e
SirMorfield <jkoers@student.codam.nl>
2022-04-21 21:20:53 +0200

    feat: finalize tester setup

--- testing/send_many_requests.sh
+++ testing/send_many_requests.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
-# log all commands exececuted
-set -x
-
 # exit when a command fails
 set -e
+export TERM=xterm-256color
+
+test_upload () {
+	curl -X POST -F "upfile=@$1" localhost:8081 &> /dev/null
+	curl -H transfer-encoding:chunked -X POST -F "upfile=@$1" -v localhost:8081 &> /dev/null
+}
+
+runner () {
+	clear
+	echo "$@"
+	eval "$@"
+}
+
 ./webserv config_file.conf > /tmp/webserv.out &
-sleep 3
 
-curl -v localhost:8081
-# curl -v localhost:8081/cgi/input
+# START TESTS
+runner curl -v localhost:8081
+runner curl -v localhost:8081/cgi/input
+runner curl -v -X POST -F "upfile=@webserv" localhost:8081
+runner curl -H transfer-encoding:chunked -X POST -F "upfile=@webserv" -v localhost:8081
+# END TESTS
 
+clear
 pkill webserv
-
-grep -c ^processor /proc/cpuinfo
-
 exit 0

63deb345911d556f9a7a0622403e4aade01387ba
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2022-06-13 16:39:11 +0200

    fix: remove newline

--- www/cgi/read_input.cpp
+++ www/cgi/read_input.cpp
@@ -13,7 +13,7 @@ int main() {
 	ssize_t n = read(0, buf, sizeof(buf));
 	if (n >= 0)
 		buf[n] = '\0';
-	std::cout << "Content-type: text/plain\r\n\r\n " << std::endl;
+	std::cout << "Content-type: text/plain\r\n\r\n " << std::ends;
 	std::cout << "Read " << n << " bytes from stdin:\n<" << std::string(buf) << ">\n " << std::endl;
 	return 0;
 }
